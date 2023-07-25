24dffb7e8e6a4eaa7a918832fb5904bb08da6929
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-07-25 14:51:52 +0530

    Find-Duplicate-Number.java

---
+++ Arrays/Find-Duplicate-Number.java
@@ -0,0 +1,15 @@
+ /*
+          Arrays.sort(nums);
+        int ans = 0;
+        for(int i =1;i<nums.length;i++){
+            if(nums[i-1] != nums[i]){
+                continue;
+            }else{
+                ans = nums[i];
+            }
+        }
+        return ans;
+        
+         */ 
+         // the above code can solve the question and give the result but time complexity is O(nLogn + n)   
+      // see the below code in O(N) .
