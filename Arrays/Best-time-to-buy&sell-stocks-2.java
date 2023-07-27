69c1afb6c1de11061fc0de17fbb239c5be4d918e
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-07-27 22:36:20 +0530

    Update Best-time-to-buy&sell-stocks-2.java

--- Arrays/Best-time-to-buy&sell-stocks-2.java
+++ Arrays/Best-time-to-buy&sell-stocks-2.java
@@ -1,1 +1,17 @@
 
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+
+       ArrayList<Integer> priceGain = new ArrayList<Integer>();
+        
+        for(int idx = 0 ; idx < prices.length-1; idx++){
+            
+            if( prices[idx] < prices[idx+1] ){
+                priceGain.add( prices[idx+1]- prices[idx]);
+            }
+                
+        }
+        return priceGain.stream().mapToInt(n->n).sum();
+    }
+}
