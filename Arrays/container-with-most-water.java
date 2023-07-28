76728d38092bb9bb357fa8e152bc28b5cf900592
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-07-28 17:54:07 +0530

    great question

--- Arrays/container-with-most-water.java
+++ Arrays/container-with-most-water.java
@@ -1,1 +1,22 @@
+// brute force approach would take O(N^2) time 
 
+// this will take O(n) time and no space
+
+class Solution {
+    public int maxArea(int[] height) {
+          
+          int left = 0, right = height.length-1 ; int maxarea = 0; 
+         while(left<right){
+             int area = Math.min(height[left],height[right])*(right-left); 
+             maxarea = Math.max(maxarea,area);
+             if(height[left]<height[right]){
+                 left++;
+             }else{
+                 right--;
+             }
+             }
+             return maxarea;
+         
+         
+    }
+}
