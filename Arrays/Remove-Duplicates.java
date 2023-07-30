d933a991db00c5f96eb83cfa53079f9fef17483f
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-07-30 11:07:22 +0530

    Update Remove-Duplicates.java

--- Arrays/Remove-Duplicates.java
+++ Arrays/Remove-Duplicates.java
@@ -1,4 +1,11 @@
 // in this question you have to tell basically how many duplicates are there in this question 
+
+/*
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/ 
 class Solution {
     public int removeDuplicates(int[] nums) {
         if(nums.length==0){
@@ -15,3 +22,5 @@ class Solution {
       // we have return i+1 , because it is said in function and in question also that how many element left after removing the duplicate elements   
     }
 }
+
+
