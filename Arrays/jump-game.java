0e467cacede9ddec9e39b2217f42631859e150ae
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-07-30 20:16:59 +0530

    Update jump-game.java

--- Arrays/jump-game.java
+++ Arrays/jump-game.java
@@ -1,1 +1,24 @@
 
+/*
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+*/
+// I am confused why : return false when (i>max) 
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+
+int n = nums.length ;
+int max =0 ;
+for(int i =0; i< n ; i++){
+    if(i>max){return false ;}
+    max = Math.max(max  ,i+nums[i]) ;
+}
+return true;
+
+
+
+        
+    }
+}
