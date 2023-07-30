15166e1d35ccef1bd552644b303c766974995617
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-07-30 11:09:23 +0530

    Update Subarray-Sum-Equals-K.java

--- Arrays/Subarray-Sum-Equals-K.java
+++ Arrays/Subarray-Sum-Equals-K.java
@@ -1,3 +1,8 @@
+/* Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+Input: nums = [1,1,1], k = 2
+Output: 2
+*/ 
+
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
