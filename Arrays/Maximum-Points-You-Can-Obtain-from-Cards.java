a5942ba7ad536308d4c0a45e039afce93d4e84ec
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-07-30 09:43:01 +0530

    Update Maximum-Points-You-Can-Obtain-from-Cards.java

--- Arrays/Maximum-Points-You-Can-Obtain-from-Cards.java
+++ Arrays/Maximum-Points-You-Can-Obtain-from-Cards.java
@@ -1,31 +1,18 @@
 
+//  this is the approach of sliding window  
+
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int res = 0;
-        int n = cardPoints.length;
-        int sum = 0;
-        for(int i = 0;i<k;i++) sum+= cardPoints[i];
 
-        int leftindex = k-1;
-        if(leftindex == n-1) return sum;
-
-        int rightindex = n-1;
-        int rightsum = 0;
-        int leftsum = 0;
-        while(k-->0 && leftindex < rightindex){
-            rightsum+= cardPoints[rightindex--];
-            leftsum+= cardPoints[leftindex--];
-            if(rightsum>leftsum){
-                sum-=leftsum;
-                sum+=rightsum;
-                leftsum = 0;
-                rightsum = 0;
-            }
-        } 
-
-
-   return sum;
-
+            int total = 0;
+        for (int i = 0; i < k; i++) total += cardPoints[i];
+        int best = total;
+        for (int i = k - 1, j = cardPoints.length - 1; i >= 0; i--, j--) {
+            total += cardPoints[j] - cardPoints[i];
+            best = Math.max(best, total);
+        }
+        return best;
+        
 
     }
 }
