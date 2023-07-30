fa4ce4abe7c4ad45c835ee1ef7bc96075720f5ff
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-07-30 17:32:24 +0530

    Update word-search.java

--- Arrays/word-search.java
+++ Arrays/word-search.java
@@ -1,1 +1,6 @@
-
+//  this is a amazing question 
+// search a particular word given as a target in a 2d matrix of characters. Return true if the target exists .
+/*
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+*/
