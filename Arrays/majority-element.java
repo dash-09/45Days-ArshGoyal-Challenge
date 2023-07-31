0f55178e2d52816f4261df14de2b81300fd9ad95
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-01 01:16:19 +0530

    Update majority-element.java

--- Arrays/majority-element.java
+++ Arrays/majority-element.java
@@ -1,1 +1,30 @@
+// moore's voting algorithm >> famous algo . 
 
+class Solution {
+    public int majorityElement(int[] nums) {
+
+if (nums.length == 1) {
+		return nums[0];
+	}
+
+int count = 0; int results =0 ;
+
+for(int i = 0; i< nums.length ; i++){
+    if(count == 0 ){
+        results = nums[i] ;
+        count = 1 ;
+
+    }else if(results== nums[i]){
+        count++ ;
+
+    } else{
+        count -- ;
+
+    }
+
+
+}
+return results ;    
+
+    }
+}
