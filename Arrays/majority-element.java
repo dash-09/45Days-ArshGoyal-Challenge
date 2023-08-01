a7c1f6ab21a4d47a17c96026bc7a17dbd40f5b28
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-01 16:15:18 +0530

    Update majority-element.java

--- Arrays/majority-element.java
+++ Arrays/majority-element.java
@@ -3,9 +3,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-if (nums.length == 1) {
-		return nums[0];
-	}
+if (nums.length == 1) {return nums[0];}
 
 int count = 0; int results =0 ;
 
@@ -13,18 +11,12 @@ for(int i = 0; i< nums.length ; i++){
     if(count == 0 ){
         results = nums[i] ;
         count = 1 ;
-
     }else if(results== nums[i]){
         count++ ;
-
     } else{
         count -- ;
-
     }
-
-
 }
-return results ;    
-
+return results ;
     }
 }
