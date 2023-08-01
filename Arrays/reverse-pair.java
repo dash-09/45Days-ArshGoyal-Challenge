8596014fb87019c7d6b0d74b5a558b7340e43a8c
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-01 22:18:17 +0530

    Update reverse-pair.java

--- Arrays/reverse-pair.java
+++ Arrays/reverse-pair.java
@@ -1,1 +1,68 @@
+/*
+Given an integer array nums, return the number of reverse pairs in the array.
 
+A reverse pair is a pair (i, j) where:
+
+0 <= i < j < nums.length and
+nums[i] > 2 * nums[j].
+ 
+
+Example 1:
+
+Input: nums = [1,3,2,3,1]
+Output: 2
+Explanation: The reverse pairs are:
+(1, 4) --> nums[1] = 3, nums[4] = 1, 3 > 2 * 1
+(3, 4) --> nums[3] = 3, nums[4] = 1, 3 > 2 * 1
+*/ 
+
+
+
+class Solution {
+    public int reversePairs(int[] nums) {
+        
+        return mergeSort(nums,0,nums.length-1);
+    }
+    static int merge(int[] nums, int low, int mid, int high) {
+        int cnt = 0;
+        int j = mid + 1; 
+        for(int i = low;i<=mid;i++) {
+            while(j<=high && nums[i] > (2 * (long) nums[j])) {
+                j++;
+            }
+            cnt += (j - (mid+1));
+        }
+        
+        ArrayList<Integer> temp = new ArrayList<>(); 
+        int left = low, right = mid+1; 
+        while(left <= mid && right<=high) {
+            if(nums[left]<=nums[right]) {
+                temp.add(nums[left++]); 
+            }
+            else {
+                temp.add(nums[right++]); 
+            }
+        }
+        
+        while(left<=mid) {
+            temp.add(nums[left++]); 
+        }
+        while(right<=high) {
+            temp.add(nums[right++]); 
+        }
+        
+        for(int i = low; i<=high;i++) {
+            nums[i] = temp.get(i - low); 
+        }
+        return cnt; 
+    }
+    static int mergeSort(int[] nums, int low, int high) {
+        if(low>=high) return 0; 
+        int mid = (low + high) / 2;
+        int inv = mergeSort(nums, low, mid); 
+        inv += mergeSort(nums, mid+1, high); 
+        inv += merge(nums, low, mid, high); 
+        return inv; 
+    }
+    
+    }
