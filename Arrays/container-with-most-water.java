eeed57c9a5acb603450bcc775718ffd46af3ab9d
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-06 09:21:22 +0530

    Update container-with-most-water.java

--- Arrays/container-with-most-water.java
+++ Arrays/container-with-most-water.java
@@ -1,6 +1,11 @@
 // brute force approach would take O(N^2) time 
-
 // this will take O(n) time and no space
+/*
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, 
+the max area of water (blue section) the container can contain is 49.
+*/
 
 class Solution {
     public int maxArea(int[] height) {
