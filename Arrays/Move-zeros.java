8b898267dc9d4c01793868f60e286b2ce4fbe44f
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-07 21:39:20 +0530

    Update Move-zeros.java

--- Arrays/Move-zeros.java
+++ Arrays/Move-zeros.java
@@ -1,8 +1,9 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 
-   int count = 0;
+         int count = 0;
         int n = nums.length;
+        
         for (int i = 0; i < n; i++){
             if(nums[i] != 0){
                 nums[count] = nums[i];
