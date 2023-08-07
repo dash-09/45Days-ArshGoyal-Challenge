4d4719c0f79a929db31d8f441c24bc59861b032e
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-07 21:33:14 +0530

    Update Maximum-Points-You-Can-Obtain-from-Cards.java

--- Arrays/Maximum-Points-You-Can-Obtain-from-Cards.java
+++ Arrays/Maximum-Points-You-Can-Obtain-from-Cards.java
@@ -1,5 +1,12 @@
 
 //  this is the approach of sliding window  
+/*
+Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+Output: 12
+Explanation: After the first step, your score will always be 1. However, choosing the rightmost card first will
+maximize your total score. The optimal strategy is to take the 
+three cards on the right, giving a final score of 1 + 6 + 5 = 12.
+*/
 
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
