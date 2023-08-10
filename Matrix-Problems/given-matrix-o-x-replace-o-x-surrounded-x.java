cc7ed57b645f5787db5cc295fb9ea6f53cb89f6c
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-10 07:22:24 +0530

    Update given-matrix-o-x-replace-o-x-surrounded-x.java

--- Matrix-Problems/given-matrix-o-x-replace-o-x-surrounded-x.java
+++ Matrix-Problems/given-matrix-o-x-replace-o-x-surrounded-x.java
@@ -1,1 +1,18 @@
 
+//  Given a matrix of ‘O’ and ‘X’, replace ‘O’ with ‘X’ if surrounded by ‘X’.
+/*
+Input: mat[M][N] =  {{'X', 'O', 'X', 'X', 'X', 'X'},
+                     {'X', 'O', 'X', 'X', 'O', 'X'},
+                     {'X', 'X', 'X', 'O', 'O', 'X'},
+                     {'O', 'X', 'X', 'X', 'X', 'X'},
+                     {'X', 'X', 'X', 'O', 'X', 'O'},
+                     {'O', 'O', 'X', 'O', 'O', 'O'},
+                    };
+Output: mat[M][N] =  {{'X', 'O', 'X', 'X', 'X', 'X'},
+                      {'X', 'O', 'X', 'X', 'X', 'X'},
+                      {'X', 'X', 'X', 'X', 'X', 'X'},
+                      {'O', 'X', 'X', 'X', 'X', 'X'},
+                      {'X', 'X', 'X', 'O', 'X', 'O'},
+                      {'O', 'O', 'X', 'O', 'O', 'O'},
+                    };
+*/ 
