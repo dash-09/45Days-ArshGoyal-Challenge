5c94385bc4d2830b9eb46927c570f48429d8091d
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-10 07:10:26 +0530

    Update rotate-image.java

--- Matrix-Problems/rotate-image.java
+++ Matrix-Problems/rotate-image.java
@@ -1,1 +1,33 @@
-
+//  this is algo that tells in order to rotate the image you have to do two things >> 1) transpose and 2) row-reverse .
+// grid has the size of N*N .
+class solution{
+   public void rotateImage(int matrix[][]){
+      transpose(matrix);
+      rowReverse(matrix);
+   }
+  public static void transpose(int matrix[][]){
+    int n = matrix.length;
+    for(int i = 0;i<n;i++){
+     for(int j = 0;j<i;j++){
+     int temp = matrix[i][j];
+       int temp = matrix[i][j] ;
+       matrix[i][j] = matrix[j][i];
+       matrix[j][i] = temp;
+     }
+    }
+  }
+  public static void rowReverse(int matrix[][]){
+    int n = matrix.length;
+    for(int i = 0;i<n;i++){
+     int left = 0;
+      int right = matrix.length-1;
+      while(left<right){
+        int temp = matrix[i][left];
+        matrix[i][left] = matrix[i][right];
+        matrix[i][right] = temp;
+        left++;
+        right--;
+      }
+    }
+  }
+}
