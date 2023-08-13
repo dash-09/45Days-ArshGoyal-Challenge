b089353cba5078d027f8f6c44cc47f8568bb4e8d
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-13 13:42:21 +0530

    Update rotate-image.java

--- Matrix-Problems/rotate-image.java
+++ Matrix-Problems/rotate-image.java
@@ -8,8 +8,7 @@ class solution{
   public static void transpose(int matrix[][]){
     int n = matrix.length;
     for(int i = 0;i<n;i++){
-     for(int j = 0;j<i;j++){
-     int temp = matrix[i][j];
+     for(int j = 0;j<i;j++){      
        int temp = matrix[i][j] ;
        matrix[i][j] = matrix[j][i];
        matrix[j][i] = temp;
