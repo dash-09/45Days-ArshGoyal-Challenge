a89b68031788e419dc0d0f8a2c3308a4d2658727
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-13 13:23:52 +0530

    Update find-all-duplicates-in-an-array.java

--- Arrays/find-all-duplicates-in-an-array.java
+++ Arrays/find-all-duplicates-in-an-array.java
@@ -1,5 +1,6 @@
 /*
-- Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+- Given an integer array nums of length n where all the integers of nums are in the range [1, n] and
+each integer appears once or twice, return an array of all the integers that appears twice.
 
 - You must write an algorithm that runs in O(n) time and uses only constant extra space.
 Input: nums = [4,3,2,7,8,2,3,1]
