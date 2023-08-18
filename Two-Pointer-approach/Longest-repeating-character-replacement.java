731ba163746ea40110dcbe96a1ba9a8d2f777428
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-18 15:49:33 +0530

    Update Longest-repeating-character-replacement.java

--- Two-Pointer-approach/Longest-repeating-character-replacement.java
+++ Two-Pointer-approach/Longest-repeating-character-replacement.java
@@ -10,3 +10,22 @@ Input: s = "ABAB", k = 2
 Output: 4
 Explanation: Replace the two 'A's with two 'B's or vice versa.
 */ 
+
+ 
+class Solution {
+  public int characterReplacement(String s, int k) {
+    // Space O(26)
+    int[] dic = new int[26];
+    int start = 0;
+    int maxLen = 0;
+    // Time O(n)
+    for (int end = 0; end < s.length(); end++) {
+      maxLen = Math.max(maxLen, ++dic[s.charAt(end) - 'A']);
+      if (end - start + 1 > maxLen + k) {
+        dic[s.charAt(start) - 'A']--;
+        start++;
+      }
+    }
+    return s.length() - start;
+  }
+}
