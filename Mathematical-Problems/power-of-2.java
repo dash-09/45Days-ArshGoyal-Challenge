52388dfaabb7e87e746f159a99108edb368628f1
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-20 15:17:56 +0530

    Update power-of-2.java

--- Mathematical-Problems/power-of-2.java
+++ Mathematical-Problems/power-of-2.java
@@ -1,1 +1,16 @@
-
+// this is the question of bit manipulation 
+/*
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+=----------------------
+Input: n = 16
+Output: true
+Explanation: 2^4 = 16
+*/ 
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+     return n>0 &&  Integer.bitCount(n)==1;
+    }
+   
+}
