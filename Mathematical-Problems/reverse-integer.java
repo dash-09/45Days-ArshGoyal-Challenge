96f62ac3ee6f86cb9d423c80d01a5d0aa7927733
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-20 10:53:29 +0530

    Update reverse-integer.java

--- Mathematical-Problems/reverse-integer.java
+++ Mathematical-Problems/reverse-integer.java
@@ -1,1 +1,20 @@
 
+/*Input: x = 123
+Output: 321
+*/ 
+class solution{
+  public static int reverseInteger(int x){
+    int res = 0;
+    while(x!=0){
+      int rem = x%10;
+      int rest = res*10+rem;
+      if((rest-rem)/10 != res){
+        return 0;
+      }
+      res = rest;
+      x = x/10;
+    }
+    return res;
+  }
+}
+    
