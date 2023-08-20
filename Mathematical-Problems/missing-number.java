d68a5cddde978757f10c9b5856e156dfafa254d3
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-20 10:35:17 +0530

    Update missing-number.java

--- Mathematical-Problems/missing-number.java
+++ Mathematical-Problems/missing-number.java
@@ -1,1 +1,11 @@
 
+class solution{
+  public static int missed(int nums[]){
+    int n = nums.length;
+    int sum = (n*(n+1)/2);
+    for(int i = 0; i<n;i++){
+      sum = sum - nums[i]; 
+    }
+    return sum;
+  }
+}
