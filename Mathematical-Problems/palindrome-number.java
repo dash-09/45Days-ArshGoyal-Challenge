1f29e1304c54181794d61d5ffd10afc39fb59b5f
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-20 10:45:29 +0530

    Update palindrome-number.java

--- Mathematical-Problems/palindrome-number.java
+++ Mathematical-Problems/palindrome-number.java
@@ -1,1 +1,16 @@
 
+// return true if the given number is palindrome .
+class solution{
+  public static boolean palindrome(int x){
+    if(x%10==0 || x<0 ){return false;}
+    int temp=z;
+    int res = 0;
+    while(temp!=0){
+      int rem = temp%10;
+      temp = temp/10;
+      res = res*10 + rem;
+    }
+    return x==res;
+    
+  }    
+}
