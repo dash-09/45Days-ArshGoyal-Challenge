21d8263d3e3aa30a3df92bc8b4f7f0e501f64cb0
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-26 15:53:58 +0530

    Update product-of-array-except-itself.java

--- Searching-Sorting/product-of-array-except-itself.java
+++ Searching-Sorting/product-of-array-except-itself.java
@@ -4,3 +4,21 @@
  /*
  */
 
+class solution{
+  public int findPeakElement(int[] nums) {
+     if(nums.length == 1){ return 0; }
+ int high = nums.length-1 ;
+  if(nums[0]> nums[1]){return 0 ;}
+  if(nums[high]> nums[high-1]){return high ;}
+   
+   int low  = 1; int high = nums.length-1;
+   while(low<high){
+    int mid = low+(high-low)/2;
+    if(nums[mid-1]<nums[mid]&& nums[mid]>nums[mid]){return mid;}
+    else if(nums[mid-1]>nums[mid]){high = mid-1;}
+    else if(nums[mid+1]>nums[mid]){low = mid+1;}
+   }
+   return -1;
+  }
+}
+
