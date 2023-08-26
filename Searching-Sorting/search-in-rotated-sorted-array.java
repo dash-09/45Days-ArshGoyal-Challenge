29b159ebfe785ad18852e9ff8f08c8c4e4afcdbe
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-08-26 16:31:05 +0530

    Update search-in-rotated-sorted-array.java

--- Searching-Sorting/search-in-rotated-sorted-array.java
+++ Searching-Sorting/search-in-rotated-sorted-array.java
@@ -1,1 +1,29 @@
 
+class Solution {
+    public int search(int[] nums, int target) {
+        
+// remember the question have a catch and that is , the input array don't have duplicates 
+        int start = 0 ; int end = nums.length -1 ;
+        while(start<=end){
+            int mid =(start+end)/2 ;
+            if(nums[mid] == target){
+                return mid ;
+            }else if(nums[mid]>= nums[start]){
+                if(nums[start] <= target && target < nums[mid]){
+                    end = mid-1 ;
+                }else{
+                    start = mid+1 ;
+                }
+
+            }else{
+                if(nums[mid]< target && target <= nums[end]){
+                    start = mid+1 ;
+                }else{
+                    end = mid-1 ;
+                }
+            }
+        }
+        return -1 ;
+    }
+
+}
