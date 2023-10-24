a8fad31c803d620404820c9007f13743a6d9b640
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-10-24 09:55:12 +0530

    Update 2find-the-index-of-the-first-occurrence-in-a-string.java

--- Strings/2find-the-index-of-the-first-occurrence-in-a-string.java
+++ Strings/2find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,1 +1,11 @@
+/*
+input : st1 = "sadbutsad"; st2 = "but" ; 
+output: 3;
+*/ 
 
+class solution{
+   public static int indexx(String haystack, String needle){
+     if(haystack.contains(needle)){return haystack.indexOf(needle));
+                                   return -1;
+   }
+}
