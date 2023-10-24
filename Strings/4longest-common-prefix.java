3d72cff11a9d448ee46aa996ca78c34ecd645ca4
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-10-24 10:29:59 +0530

    Update 4longest-common-prefix.java

--- Strings/4longest-common-prefix.java
+++ Strings/4longest-common-prefix.java
@@ -1,1 +1,23 @@
 
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+      
+        Arrays.sort(strs);
+        StringBuilder sb = new StringBuilder();
+          String x = strs[0];
+          String y = strs[strs.length-1];
+            int i = 0;
+
+        while(i<x.length()&& i<y.length()){
+            if(x.charAt(i)==y.charAt(i)){
+                sb.append(x.charAt(i));
+                i++;
+            }else{
+                break;
+            }
+
+
+        }
+    return sb.toString();
+    }
+}
