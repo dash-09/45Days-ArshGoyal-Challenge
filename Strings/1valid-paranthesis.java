fa956ca4e0134140b14555507c3cf8f22be74127
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-10-24 09:30:02 +0530

    Update 1valid-paranthesis.java

--- Strings/1valid-paranthesis.java
+++ Strings/1valid-paranthesis.java
@@ -1,1 +1,16 @@
 
+class solution{
+  public static boolean validParanthesis(String st){
+    Stack<Character> stak = new Stack<>();
+    for(char c: st.toCharArray()){
+    if(c=='('){stak.push(')');}
+    else if(c=='['){stak.push(']');} 
+    else if(c=='{'){stak.push('}');}
+    else if(stak.isEmpty() || stak.pop() != c){return false;}  
+      
+    }
+    return stak.isEmpty();
+
+    
+  }  
+}
