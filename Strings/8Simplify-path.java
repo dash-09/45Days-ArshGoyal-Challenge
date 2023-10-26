3bac3e3606a392d71ef66a23470312787fa44d5f
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-10-26 16:47:15 +0530

    Update 8Simplify-path.java

--- Strings/8Simplify-path.java
+++ Strings/8Simplify-path.java
@@ -1,1 +1,17 @@
 
+class solution{
+  public String simplePath(String s ){
+     String store[] = s.split("/");
+    Stack<String> st = new Stack<>();
+
+    for(final String temp : store){
+         if(temp.isEmpty() || temp.equals(".")) continue;
+         if(temp.equals("..")){
+            if(!st.isEmpty()){
+              st.pop();
+            }             
+         } else{st.push(temp);}  
+    } 
+    return "/" + String.join("/", st);
+  }
+}
