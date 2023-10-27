7db91a694d5619d9cf080b64a0a4e19bdb2f8131
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-10-27 07:47:13 +0530

    Update 10Group-anagrams.java

--- Strings/10Group-anagrams.java
+++ Strings/10Group-anagrams.java
@@ -1,1 +1,29 @@
 
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+   Map<String, List<String>> map = new HashMap<>();
+        
+        for(int i = 0;i<strs.length;i++){
+            String temp = sortString(strs[i]);
+            if(!map.containsKey(temp)){
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                map.put(temp,list);
+            }else{
+                map.get(temp).add(strs[i]);
+            }
+        }
+
+         List<List<String>> list = new ArrayList<>();
+        for(List<String> trs : map.values()){
+            list.add(trs);
+        }
+        return list;
+
+    }
+  public static String sortString(String s){
+        char a[] = s.toCharArray(); Arrays.sort(a);
+       return new String(a);
+    } 
+}
