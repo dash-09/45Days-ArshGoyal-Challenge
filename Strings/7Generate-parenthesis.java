72e9c09c9a57a5531ccce7239b6edd4d5b4c2589
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-10-28 10:56:53 +0530

    Update 7Generate-parenthesis.java

--- Strings/7Generate-parenthesis.java
+++ Strings/7Generate-parenthesis.java
@@ -1,1 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
 
+      List<String> list = new ArrayList<>();
+    funtionGenerate(list,"",0,0,  n );
+    return list;
+   
+
+
+
+    }
+    public void funtionGenerate(List<String> list, String s, int open , int close, int n){
+          if(s.length()==n*2){
+        list.add(s); return;
+      }
+
+    if(open<n){
+      funtionGenerate(list, s + "(", open+1, close, n);
+    }
+    if(close<open){
+      funtionGenerate(list, s + ")", open, close+1,n);
+    }
+    }
+
+}
