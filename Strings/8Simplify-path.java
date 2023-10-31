ddadaa71051056737dfb680fbf9d12ad59ba75ef
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2023-10-31 15:50:09 +0530

    Update 8Simplify-path.java

--- Strings/8Simplify-path.java
+++ Strings/8Simplify-path.java
@@ -1,3 +1,9 @@
+/*
+Input: path = "/home/"
+Output: "/home"
+Explanation: Note that there is no trailing slash after the last directory name.
+*/
+
 
 class solution{
   public String simplePath(String s ){
